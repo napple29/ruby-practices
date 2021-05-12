9c1db8504c4ad7238b7dc5bab48c7061f68527f9
haruna <snwxxx29@gmail.com>
2021-05-12 16:12:53 +0900

    Add fizzbuzz-practice

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,1 +1,13 @@
-puts "fuzzbuzz!"
+x = 0
+while x <= 19 do
+    x += 1
+    if x % 3 == 0 && x % 5 ==0
+        puts "FizzBuzz"
+    elsif x % 3 == 0
+        puts "Fizz"
+    elsif x % 5 == 0
+        puts "Buzz"
+    else
+        puts x
+    end
+end
