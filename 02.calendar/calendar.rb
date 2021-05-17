aec24865b082f74582c5769a1822136147c851da
haruna <snwxxx29@gmail.com>
2021-05-18 08:10:56 +0900

    Change calendar

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -5,13 +5,8 @@ month_year = ARGV.getopts('m:', 'y:')
 year = month_year["y"].to_i
 month = month_year["m"].to_i
 
-if year == 0
-    year = Date.today.year
-end
-
-if month == 0
-    month = Date.today.month
-end
+year = Date.today.year if year == 0
+month = Date.today.month if month == 0
 
 head = "#{month}月 #{year}"
 puts head.center(20)
@@ -20,18 +15,16 @@ puts "日 月 火 水 木 金 土"
 first_day = Date.new(year, month , 1);
 last_day = Date.new(year, month , -1); 
 
-count = first_day.wday
 space = "   " * first_day.wday
 print space
 
-range = first_day.day..last_day.day
+range = first_day..last_day
 range.each do |day|
-    print day.to_s.rjust(2)
-    print " "
-    count = count + 1
-    if (count % 7 == 0)
-        print("\n")
-    end
+  print day.day.to_s.rjust(2)
+  print " "
+  if day.saturday?
+    print "\n"
+  end
 end
 puts ""
 
