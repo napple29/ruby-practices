5634d6a84fa95d01039215b427e98ec71a1177b2
napple29 <snwxxx29@gmail.com>
2021-10-20 14:07:45 +0900

    クラスを継承後修正

--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -2,12 +2,11 @@ require_relative './frame.rb'
 
 class Game < Frame
   def calc_score
-
     total = 0
     shots_into_frame[0..8].each.with_index(1) do |frame, index|
       total += 
         if frame[0] == 10
-          shots_into_frame[index][1]? 10 + shots_into_frame[index][0] + shots_into_frame[index][1] : 10 + game.shots_into_frame[index][0] + game.shots_into_frame[index + 1][0]
+          shots_into_frame[index][1]? 10 + shots_into_frame[index][0] + shots_into_frame[index][1] : 10 + shots_into_frame[index][0] + shots_into_frame[index + 1][0]
         elsif frame.sum == 10
           10 + shots_into_frame[index][0]
         else
