dd59cda363a61fd7dad98385a4b348df5aa0c7e2
napple29 <snwxxx29@gmail.com>
2021-10-29 15:43:01 +0900

    変数名を変更

--- 08.ls_object/detail.rb
+++ 08.ls_object/detail.rb
@@ -13,19 +13,22 @@ class Detail
 
   def self.output_nomal_option(files)
     column = 3
-    number = if (files.size % column).zero?
+    line = if (files.size % column).zero?
                files.size / column
              else
                files.size / column + 1
              end
 
-    divide_file = files.each_slice(number).to_a
+    divide_by_colmuns = files.each_slice(line).to_a
 
-    (divide_file.first.size - divide_file.last.size).times { divide_file.last.push('') } unless divide_file.last.size == divide_file.first.size
+    unless divide_by_colmuns.last.size == divide_by_colmuns.first.size
+      (divide_by_colmuns.first.size - divide_by_colmuns.last.size).times { divide_by_colmuns.last.push('') }
+    end
+
     between_files = files.max_by(&:size).size + 10
 
-    divide_file.transpose.each do |files|
-      files.each_with_index do |file, idx|
+    divide_by_colmuns.transpose.each do |divide_by_colmun|
+      divide_by_colmun.each_with_index do |file, idx|
         print file + ' ' * (between_files - file.size)
         print "\n" if ((idx + 1) % column).zero?
       end
